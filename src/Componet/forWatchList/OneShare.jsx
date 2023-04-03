d61d53b1f515bb03d388c6ad61edb7fd738c81ae
Nilesh <sariyanilesh7265@gmail.com>
2023-04-04 01:28:19 +0530

    Draggagle model is added. when Buy button is click

--- src/Componet/forWatchList/OneShare.jsx
+++ src/Componet/forWatchList/OneShare.jsx
@@ -1,19 +1,31 @@
-import React,{useState} from "react"; 
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
-import { makeStyles } from '@material-ui/core/styles';
-import Box from '@mui/material/Box'; 
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import Draggable from "react-draggable";
+// import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles({
   smallButton: {
-    width: '30px',
-    padding:'0px',
-    margin:'0px',
-    height: '28px',
+    width: "30px",
+    padding: "0px",
+    margin: "0px",
+    height: "28px",
   },
 });
 
+const myStyles = makeStyles(() => ({
+  backdrop: {
+    // backgroundColor: 'rgba(255, 255, 255, 0.5)',
+  },
+}));
+
+function CustomBackdrop(props) {
+  const classes = useStyles();
+  return <div className={classes.backdrop} {...props} />;
+}
 // const style = {
 //   position: 'absolute',
 //   top: '50%',
@@ -26,9 +38,6 @@ const useStyles = makeStyles({
 //   p: 4,
 // };
 
-
-
-
 function OneShare(props) {
   const { shareName, absolutePrice, percentegePrice, lastPrice } = props;
   const classes = useStyles();
@@ -36,74 +45,76 @@ function OneShare(props) {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
- 
-  const modalStyle = {
-    top: `${position.y}%`,
-    left: `${position.x}%`,
-    transform: `translate(-${position.x}%, -${position.y}%)`,
-  };
+
+  // const modalStyle = {
+  //   top: `${position.y}%`,
+  //   left: `${position.x}%`,
+  //   transform: `translate(-${position.x}%, -${position.y}%)`,
+  // };
 
   const style = {
-  position: 'absolute',
-  // top: '50%',
-  // left: '50%',
-  // transform: 'translate(-50%, -50%)',
-  top: `${position.y}%`,
-  left: `${position.x}%`,
-  transform: `translate(-${position.x}%, -${position.y}%)`,
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  color: 'white',
-  p: 4,
-};
-  const handleDragStart = (e) => {
-    e.preventDefault();
-    const startX = e.clientX;
-    const startY = e.clientY;
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    // top: `${position.y}%`,
+    // left: `${position.x}%`,
+    // transform: `translate(-${position.x}%, -${position.y}%)`,
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
 
-    const onMouseMove = (e) => {
-      const deltaX = e.clientX - startX;
-      const deltaY = e.clientY - startY;
-      setPosition((prevPosition) => ({
-        x: prevPosition.x + deltaX,
-        y: prevPosition.y + deltaY,
-      }));
-    };
-
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', () => {
-    document.removeEventListener('mousemove', onMouseMove);
-    });
+    // color: 'white',
+    p: 4,
   };
+  // const handleDragStart = (e) => {
+  //   e.preventDefault();
+  //   const startX = e.clientX;
+  //   const startY = e.clientY;
+
+  //   const onMouseMove = (e) => {
+  //     const deltaX = e.clientX - startX;
+  //     const deltaY = e.clientY - startY;
+  //     setPosition((prevPosition) => ({
+  //       x: prevPosition.x + deltaX,
+  //       y: prevPosition.y + deltaY,
+  //     }));
+  //   };
+
+  //   document.addEventListener('mousemove', onMouseMove);
+  //   document.addEventListener('mouseup', () => {
+  //   document.removeEventListener('mousemove', onMouseMove);
+  //   });
+  // };
 
   return (
     <>
-    
-    {/* <Button onClick={handleOpen}>Open modal</Button> */}
-      <Modal
-        open={open}
-        onClose={handleClose}
-        // style={modalStyle}
-        
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        {/* <Box sx={style} draggable onDragStart={handleDragStart}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-          
-        </Box> */}
-        <div style={style} onDragStart={handleDragStart} draggable>
+      {/* <Button onClick={handleOpen}>Open modal</Button> */}
+      <Draggable>
+        <Modal
+        BackdropComponent={CustomBackdrop} {...props}
+          open={open}
+          onClose={handleClose}
+          // style={modalStyle}
+
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+          {/* <div style={style} onDragStart={handleDragStart} draggable>
         <h1>Nilesh</h1>
         <Button onClick={handleClose}>Close</Button>
-      </div>
-      </Modal>
+      </div> */}
+        </Modal>
+      </Draggable>
 
       <div className="share-list" style={{ fontSize: "14px" }}>
         <div className="share">
@@ -116,10 +127,26 @@ function OneShare(props) {
             </div>
             <div className="buySell">
               {/* <Box sx={{ "& button": { m: 1 } }}> */}
-                <div style={{display:'flex'}}>
-                  <Button onClick={handleOpen} className={classes.smallButton} style={{marginRight:'5px'}} color="success" variant="contained" size="small">Buy</Button>
-                  <Button className={classes.smallButton} color="error" variant="contained" size="small">Sell</Button>
-                </div>
+              <div style={{ display: "flex" }}>
+                <Button
+                  onClick={handleOpen}
+                  className={classes.smallButton}
+                  style={{ marginRight: "5px" }}
+                  color="success"
+                  variant="contained"
+                  size="small"
+                >
+                  Buy
+                </Button>
+                <Button
+                  className={classes.smallButton}
+                  color="error"
+                  variant="contained"
+                  size="small"
+                >
+                  Sell
+                </Button>
+              </div>
               {/* </Box> */}
             </div>
           </div>
